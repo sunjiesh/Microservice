1de34e90ae82f2a1417741e450bcc2251bf88eec
Tom Sun <sunjie310110@gmail.com>
2016-07-11 21:59:21 +0800

    add new eurekaserver module

---
+++ microservice-parent/microservice-eurekaserver/src/main/java/cn/com/sunjiesh/microservice/eurekaserver/EurekaServiceApplication.java
@@ -0,0 +1,20 @@
+package cn.com.sunjiesh.microservice.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author Tom
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
